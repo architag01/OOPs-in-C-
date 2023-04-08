1d086df6dd8f3ec1843a9b02137952e22ab15048
Archit Agarwal <agarwalarchit03@gmail.com>
2023-04-08 20:38:58 +0530

    Create inheritance.cpp

---
+++ inheritance.cpp
@@ -0,0 +1,29 @@
+// C++ program to demonstrate implementation
+// of Inheritance
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Parent {
+public:
+	int id_p;
+};
+
+
+class Child : public Parent {
+public:
+	int id_c;
+};
+
+int main()
+{
+	Child obj1;
+
+	obj1.id_c = 7;
+	obj1.id_p = 91;
+	cout << "Child id is: " << obj1.id_c << '\n';
+	cout << "Parent id is: " << obj1.id_p << '\n';
+
+	return 0;
+}
